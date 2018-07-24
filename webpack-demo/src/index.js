889645b3c3c3ccad170a1463751dcd882b33870c
TakHyun-Kang <azx1054@gmail.com>
2018-07-24 16:00:13 +0900

    webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -3,15 +3,31 @@ import './index.scss';
 import Icon from './ico.png';
 
 function component() {
-    var element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack!!!!'], ' ');
-    element.classList.add('hello');
+    var error = document.getElementById('errorNum');
+    error.innerHTML = _.join(['0'], ' ');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    var progress = document.getElementById('progressNum');
+    progress.innerHTML = _.join(['0'], ' ');
 
-    element.appendChild(myIcon);
+    var completed = document.getElementById('completedNum');
+    completed.innerHTML = _.join(['0'], ' ');
+
+    var reserved = document.getElementById('reservedNum');
+    reserved.innerHTML = _.join(['0'], ' ');
+
+
+    var uName = document.getElementById('uName');
+    uName.innerHTML = _.join(['Jeongeun Kwon'], ' ');
+
+    var notice = document.getElementById('noticeNum');
+    notice.innerHTML = _.join(['12'], ' ');
+
+    // element.classList.add('hello');
+
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
+
+    // element.appendChild(myIcon);
   
     return element;
   }
