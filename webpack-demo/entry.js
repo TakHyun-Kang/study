d8fb5377bf007a308165baa8bb6ec32b6d555d35
TakHyun-Kang <azx1054@gmail.com>
2018-07-25 08:32:59 +0900

    webpack

--- webpack-demo/entry.js
+++
@@ -1,8 +0,0 @@
-import createDiv from './createDiv';
-
-var diceNumber = -~(Math.random() * 6);
-var diceDiv = createDiv(diceNumber);
-document.body.appendChild(div);
-if (module.hot) {
-  module.hot.accept();
-}
