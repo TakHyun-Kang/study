a5760a61cc45323561c374cdeda8234e138014ac
TakHyun-Kang <azx1054@gmail.com>
2018-07-25 07:49:10 +0900

    webpack

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -15,11 +15,39 @@ function component() {
     reserved.innerHTML = _.join(['0'], ' ');
 
 
+    var menu = function() {
+      var sMenuBox = document.getElementById('sMenuBox');
+      if (sMenuBox.style.width === "0px") {
+        document.body.style.left = '200px';
+        sMenuBox.style.width = '200px';
+      } else {
+        document.body.style.left = '0px';
+        sMenuBox.style.width = '0px';
+      }
+    }
+    var sideMenu = document.getElementById('sideMenu');
+    sideMenu.addEventListener('click', menu);
+    var closeBtn = document.getElementById('closeBtn');
+    closeBtn.addEventListener('click', menu);
+
     var uName = document.getElementById('uName');
     uName.innerHTML = _.join(['Jeongeun Kwon'], ' ');
 
     var notice = document.getElementById('noticeNum');
     notice.innerHTML = _.join(['12'], ' ');
+    
+    var info = function() {
+      var userInfo2 = document.getElementById('userInfo2');
+      if (userInfo2.style.display === "none") {
+        userInfo2.style.display = 'block';
+      } else {
+        userInfo2.style.display = 'none';
+      }
+    };
+    var userInfo = document.getElementById('userInfo');
+    userInfo.addEventListener('click', info);
+
+
 
     document.addEventListener('DOMContentLoaded', function() {
       var elems = document.querySelectorAll('.datepicker');
